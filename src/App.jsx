3c33f1f3ed923aa9ede7543408b5323b84032155
Rafael Shono <rafael.ifpr.2019@gmail.com>
2023-01-23 07:56:03 -0300

    First 3 components done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Info from './components/Info'
+import Service from './components/Service'
 import './App.css'
+import Client from './components/Client'
 
 function App() {
 
-
+//      <Info />
   return (
     <div className="App">
       <Header />
-      <Info />
+      <Info className='header' />
+      < Service />
+      <Client/>
     </div>
   )
 }
--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,8 +1,28 @@
 import React from "react"
-
+import '../css/client.css'
+import mainClient from '../assets/clients/mainClient.png'
 const Client = (props) => {
   return (
-    <div>
+    <div className="client">
+      <h2>What our happy client say</h2>
+      <p>Several selected clients, who already believe in our service</p>
+      <div className="left-client">
+      <img src={mainClient}/>
+      <div className="left-client-ajust">
+      <h3>Matthew Paul</h3>
+      <p>Perfect, very good job! Thank you for the amazing design and work. 
+        Really impressed with the high quality and quick turnaround time. Highly recommend.</p>
+        <div className="dot-group">
+        <span className="dot"></span>
+  <span className="dot"></span>
+  <span className="dot"></span>
+  <span className="dot"></span>
+  </div>
+        </div>
+      </div>
+      <div className="group-clients">
+
+      </div>
       
     </div>
   )
